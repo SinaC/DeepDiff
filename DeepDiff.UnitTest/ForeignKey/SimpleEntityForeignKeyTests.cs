e14f451310b8f2bb3da80047a7b83ebc73c1b677
SinaC <heymbeeck.joel@gmail.com>
2024-01-27 12:14:05 +0100

    1.3.6 Additional tests

--- DeepDiff.UnitTest/ForeignKey/SimpleEntityForeignKeyTests.cs
+++ DeepDiff.UnitTest/ForeignKey/SimpleEntityForeignKeyTests.cs
@@ -177,5 +177,102 @@ namespace DeepDiff.UnitTest.ForeignKey
             Assert.Single(results.Single().SubEntity.SubEntities);
             Assert.Equal(results.Single().SubEntity.Id, results.Single().SubEntity.SubEntities.Single().EntityLevel1Id); // PK has been copied to FK
         }
+
+        [Fact]
+        public void HasMany_WithinHasOne_Insert_HasNavigationKey()
+        {
+            var existingId = Guid.NewGuid();
+            var existingEntities = new[]
+            {
+                new EntityLevel0
+                {
+                    Index = 1,
+
+                    Id = existingId,
+
+                    StartsOn = DateTime.Today,
+                    Direction = Direction.Up,
+
+                    RequestedPower = 1,
+                    Penalty = 1,
+
+                    SubEntity = new EntityLevel1
+                    {
+                        Index = 1,
+
+                        Id = Guid.NewGuid(),
+
+                        Timestamp = DateTime.Today.AddMinutes(15),
+
+                        Power = 1,
+
+                        EntityLevel0Id = existingId
+                    }
+                }
+            };
+
+            var newEntities = new[]
+            {
+                new EntityLevel0
+                {
+                    Index = 1,
+
+                    Id = Guid.NewGuid(),
+
+                    StartsOn = DateTime.Today,
+                    Direction = Direction.Up,
+
+                    RequestedPower = 1,
+                    Penalty = 1,
+
+                    SubEntity = new EntityLevel1
+                    {
+                        Index = 1,
+
+                        Id = Guid.NewGuid(),
+
+                        Timestamp = DateTime.Today.AddMinutes(15),
+
+                        Power = 1,
+                        SubEntities = new List<EntityLevel2>
+                        {
+                            new EntityLevel2
+                            {
+                                Index = 1,
+
+                                Id = Guid.NewGuid(),
+
+                                DeliveryPointEan = "DPEAN",
+                                Value1 = 1,
+                            }
+                        }
+                    }
+                }
+            };
+
+            DiffConfiguration diffConfiguration = new DiffConfiguration();
+            diffConfiguration.PersistEntity<EntityLevel0>()
+                .HasKey(x => new { x.StartsOn, x.Direction })
+                .HasValues(x => new { x.RequestedPower, x.Penalty })
+                .HasOne(x => x.SubEntity, cfg => cfg.HasNavigationKey(x => x.EntityLevel0Id, x => x.Id));
+            diffConfiguration.PersistEntity<EntityLevel1>()
+                .HasKey(x => x.Timestamp)
+                .HasValues(x => new { x.Power })
+                .HasMany(x => x.SubEntities, cfg => cfg.HasNavigationKey(x => x.EntityLevel1Id, x => x.Id));
+            diffConfiguration.PersistEntity<EntityLevel2>()
+               .HasKey(x => x.DeliveryPointEan)
+               .HasValues(x => new { x.Value1, x.Value2 });
+
+            var deepDiff = diffConfiguration.CreateDeepDiff();
+            var diff = deepDiff.DiffMany(existingEntities, newEntities);
+            var results = diff.Entities.ToArray();
+
+            Assert.Single(results);
+            Assert.Equal(PersistChange.None, results.Single().PersistChange);
+            Assert.Equal(PersistChange.None, results.Single().SubEntity.PersistChange);
+            Assert.Equal(results.Single().Id, results.Single().SubEntity.EntityLevel0Id); // FK has not been overridden
+            Assert.Single(results.Single().SubEntity.SubEntities);
+            Assert.Equal(results.Single().SubEntity.Id, results.Single().SubEntity.SubEntities.Single().EntityLevel1Id); // PK has been copied to FK
+        }
     }
 }
