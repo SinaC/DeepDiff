d8ecdd04ab3f8586d63485812b310590d5020a7e
Michaël Krejci <michael@comi.be>
2025-04-07 08:45:35 +0200

    #117 : add a generic IOperationListener that can be used instead of the IOperationListener if an implementation by Entity is needed

---
+++ DeepDiff/Configuration/IOperationListenerOfT.cs
@@ -0,0 +1,10 @@
+﻿namespace DeepDiff.Configuration
+{
+    /// <summary>
+    /// Defines a generic listener that can be used instead of the IOperationListener if an implementation by Entity is needed
+    /// </summary>
+    public interface IOperationListener<T> : IOperationListener
+        where T : class
+    {
+    }
+}
